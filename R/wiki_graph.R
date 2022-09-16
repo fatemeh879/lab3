bd42eeededc3afe555204036c2a59827353533bc
fatemeh879 <sharifiasl.fateme@yahoo.com>
2022-09-16 15:14:12 +0200

    documentation is done

---
+++ R/wiki_graph.R
@@ -0,0 +1,12 @@
+#' Distances w between 6 nodes v.
+#'
+#' A dataset containing the pathway showed on top of the wikipedia page from Dijkstra's algorithm
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{point 1}
+#'   \item{v2}{point 2}
+#'   \item{w}{weight or distance from point 1 to point 2}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra's algorithm}
+"wiki_graph"
